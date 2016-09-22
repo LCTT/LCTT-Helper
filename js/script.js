3fcf6da2b8086f73ed3f5c35deb94f2c6acfed67
Bestony <best.tony@foxmail.com>
2016-09-22 17:40:07 +0800

    update script.js

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ editor.create();
 // 转换MD
 function getmd() {
     var content = toMarkdown(editor.$txt.html(), { gfm: true });
-    content = content + '\n\n--------------------------------------------------------------------------------\nvia: 网址\n\n作者：[ ][a]\n\n译者：[译者ID](https://github.com/译者ID)\n\n校对：[校对者ID](https://github.com/校对者ID)\n\n本文由 [LCTT](https://github.com/LCTT/TranslateProject) 原创编译，[Linux中国](https://linux.cn/) 荣誉推出';
+    content = content + '\n\n--------------------------------------------------------------------------------\n\nvia: 网址\n\n作者：[ ][a]\n\n译者：[译者ID](https://github.com/译者ID)\n\n校对：[校对者ID](https://github.com/校对者ID)\n\n本文由 [LCTT](https://github.com/LCTT/TranslateProject) 原创编译，[Linux中国](https://linux.cn/) 荣誉推出';
     $('#md').html(content);
     $('pre#md').each(function(i, block) {
         hljs.highlightBlock(block);
