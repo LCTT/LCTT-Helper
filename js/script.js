3cc6d87641d179c98d8ed18d033cc59bb1928c64
joriewong <jveronjorie@sina.cn>
2017-11-10 17:10:48 +0800

    auto generateMarkdown&auto copyToClipboard

--- js/script.js
+++ js/script.js
@@ -35,6 +35,11 @@ function initialize() {
 
 META_INF = '\n\n' + '-'.repeat(80) + '\n\nvia: 网址\n\n作者：[ ][a]\n译者：[译者ID](https://github.com/译者ID)\n校对：[校对者ID](https://github.com/校对者ID)\n\n本文由 [LCTT](https://github.com/LCTT/TranslateProject) 原创编译，[Linux中国](https://linux.cn/) 荣誉推出\n\n[a]:';
 
+// 自动生成MD
+editor.onchange = function() {
+    generateMarkdown();
+}
+
 function generateMarkdown() {
     initialize();
 
@@ -44,6 +49,9 @@ function generateMarkdown() {
     //通过forEach生成LinkMap，放在文件后
     linkArray.forEach(getLink);
     lctteditor.focus();
+
+    // 自动复制到剪贴板
+    copyToClipboard();
 }
 // 复制内容
 function copyToClipboard() {
